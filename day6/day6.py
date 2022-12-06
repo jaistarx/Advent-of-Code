5f807e2fe5e71fce800b52b611c880310f49c671
Jaison J <jaisonj@GMG.LOCAL>
2022-12-06 12:24:52 +0530

    completed day 6

--- day6/day6.py
+++ day6/day6.py
@@ -2,12 +2,35 @@ file = open('input.txt', 'r')
 content = file.read()
 arr=content.splitlines()
 
+arr=arr[0]
+
+from collections import Counter
 
 def part1():
-    
-part1()
+    i=0
+    j=4
+    while(j<len(arr)):
+        r=arr[i:j]
+        freq = Counter(r)
+        if(len(freq) == 4):
+            break
+        i+=1
+        j+=1
+    print(j)
+# part1()
+
+
 
 
 def part2():
-
+    i=0
+    j=14
+    while(j<len(arr)):
+        r=arr[i:j]
+        freq = Counter(r)
+        if(len(freq) == 14):
+            break
+        i+=1
+        j+=1
+    print(j)
 # part2()
---
+++ day7/day7.py
@@ -0,0 +1,14 @@
+file = open('input.txt', 'r')
+content = file.read()
+arr=content.splitlines()
+
+def part1():
+    
+part1()
+
+
+
+
+def part2():
+   
+part2()
